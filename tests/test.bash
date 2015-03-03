26489b4b287254ce7b93ac088f3daa5c831c8367
Aaron Frank <afrankz@umich.edu>
2015-03-02 20:04:47 -0500

    added new RNA parameter files and test files

--- tests/test.bash
+++ tests/test.bash
@@ -4,3 +4,17 @@ cd tests
 -reffile ../data/larmorD_reference_shifts.dat \
 -accfile ../data/larmorD_accuracy_weight.dat \
 -cutoff 15.0 A003.pdb
+echo " "
+../bin/larmord  -csfile measured_shifts_2ADT.dat \
+-parmfile ../data/larmorD_alphas_betas_rna.dat \
+-reffile ../data/larmorD_reference_shifts_rna.dat \
+-accfile ../data/larmorD_accuracy_weight_rna.dat \
+-cutoff 9999.0 2ADT.pdb
+echo " "
+../bin/larmord  -csfile measured_shifts_2ADT.dat \
+-parmfile ../data/larmorD_alphas_betas_rna.dat \
+-reffile ../data/larmorD_reference_shifts_rna.dat \
+-accfile ../data/larmorD_accuracy_weight_rna.dat \
+-printError \
+-cutoff 9999.0 2ADT.pdb
+

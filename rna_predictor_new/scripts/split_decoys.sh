9395bef2a96807f1258f2537368353b2e837c9f9
Aaron Frank <afrankz@umich.edu>
2017-10-29 13:04:00 -0400

    added decoys...
    —need to extract features from these decoys…

--- rna_predictor_new/scripts/split_decoys.sh
+++ rna_predictor_new/scripts/split_decoys.sh
@@ -2,13 +2,13 @@
 
 # script to split decoy files
 source ~/.bashrc	
-rnas="1KKA  1PJY  1Z2J  2L3E  2LI4  2LUB  2M24  2MHI  2N6S  2NBZ  4A4S 1L1W  1R7W  1ZC5  2LBJ  2LK3  2LUN  2M4W  2MNC  2N6T  2NC0  4A4T 1LC6  1SCL  2FDT  2LBL  2LP9  2LV0  2M5U  2MXL  2N6W  2NCI  4A4U 1LDZ  1UUU  2JWV  2LDL  2LPA  2M12  2M8K  2N2O  2N6X  2QH2  5A17 1NC0  1XHP  2K66  2LDT  2LQZ  2M21  2MEQ  2N2P  2N7X  2QH4  5A18 1OW9  1YSV  2KOC  2LHP  2LU0  2M22  2MFD  2N4L  2NBY  2Y95  5KQE"
+rnas="1L1W 1LC6 1LDZ 1NC0 1OW9 1PJY 1R7W 1R7Z 1UUU 1YSV 2FDT 2GM0 2JXQ 2JXS 2K3Z 2K41 2KOC 2KYD 2LBJ 2LBL 2LDL 2LDT 2LHP 2LI4 2LK3 2LP9 2LPA 2LU0 2LUB 2LV0 2RN1 2Y95 4A4S 4A4T 4A4U 2JWV 2K63 2K64 2K65 2K66 2LPS 2LQZ 2LUN 2LX1 2M12 2M21 2M22 2M23 2M24 2M57 2M8K 2MEQ 2MHI 2MI0 2MIS 1SCL 1XHP 1ZC5 2QH2 2QH3 2QH4 1Z2J 2JWV 2K63 2K64 2K65 2K66 2LPS 2LQZ 2LUN 2LX1 2M12 2M21 2M22 2M23 2M24 2M57 2M8K 2MEQ 2MHI 2MI0 2MIS 1SCL 1XHP 1ZC5 2QH2 2QH3 2QH4"
 
 # loop over rnas and get data
 for rna in ${rnas}
 do	
-	inputname=decoy_files/${rna}_nlb_decoys
-  mkdir decoy_files/${rna}
+	inputname=${rna}/${rna}_decoys_1_nlb_decoys
+  mkdir -p decoy_files/${rna}
   
 	# Count number of models using catdcd
 	models=`catdcd -pdb ${inputname}.pdb | tail -2 | head -1 | awk '{print $3}'`	
@@ -23,4 +23,4 @@ do
 		catdcd -o decoy_files/${rna}/decoy_${model}.pdb -otype pdb -s ${inputname}.pdb -stype pdb -first ${i} -last ${i} -pdb ${inputname}.pdb #> /dev/null
 	done
 done	
-fi
+

07447b38bcc4ba5db4d0c5695a6b013bdb402ca5
Aaron Frank <afrankz@umich.edu>
2017-11-06 09:12:53 -0500

    added parameter file and made training script more general

--- rna_predictor_new/scripts/train_predictor_trial.bash
+++ rna_predictor_new/scripts/train_predictor_trial.bash
@@ -1,15 +1,16 @@
 #!/bin/bash
-if [[ $# -ne 1 ]]
+if [[ $# -ne 2 ]]
 then
 	echo "Script to calculate chemical shifts"
-	echo "usage $0: <nucleus>"
+	echo "usage $0: <rna> <nucleus>"
 else
 	source ~/.bashrc	
 	
-  nucleus=$1
+  rna=$1
+  nucleus=$2
   
   # script to train chemical shift predictor for a given nucleus
   awk -v nucleus=${nucleus} '{if($1==nucleus) print}' data/initial_parameters.txt > data/initial_parameters_tmp.txt
-  cat ????/train_features_min_15.0A.txt | awk -v nucleus=${nucleus} '{ if($5==nucleus) print}' > data/train_tmp.txt   
+  cat ${rna}/train_features_min_15.0A.txt | awk -v nucleus=${nucleus} '{ if($5==nucleus) print}' > data/train_tmp.txt   
   ./scripts/train.R --verbose --ga_populations=68 --ga_cycles=1000 --output_parameters=data/ga_model_${nucleus}.txt data/train_tmp.txt
 fi

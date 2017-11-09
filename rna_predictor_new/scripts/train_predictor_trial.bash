32e4f84157868beff3f22eabf91ecebfd56a3a82
blairwhittington <blairjw@umich.edu>
2017-11-09 11:52:45 -0500

    generalized train_predictor_trial.bash
    Generalized train_predictor_trial.bash file: Added “struct” (to
    designate minimized or unminimized structure) and “r_cut” (to designate
    applied distance cut-off) options to be supplied from the command line
    
    When I initially performed the feature extraction, I added a
    descriptive line to some of the output files. Since every output file
    did not contain this line, I removed it from the files that did so that
    text file loading would be easier in the future.

--- rna_predictor_new/scripts/train_predictor_trial.bash
+++ rna_predictor_new/scripts/train_predictor_trial.bash
@@ -1,16 +1,18 @@
 #!/bin/bash
-if [[ $# -ne 2 ]]
+if [[ $# -ne 4 ]]
 then
 	echo "Script to calculate chemical shifts"
-	echo "usage $0: <rna> <nucleus>"
+	echo "usage $0: <rna> <nucleus> <struct> <r_cut>"
 else
 	source ~/.bashrc	
 	
   rna=$1
   nucleus=$2
-  
+  struct=$3
+  r_cut=$4
+
   # script to train chemical shift predictor for a given nucleus
   awk -v nucleus=${nucleus} '{if($1==nucleus) print}' data/initial_parameters.txt > data/initial_parameters_tmp.txt
-  cat ${rna}/train_features_min_15.0A.txt | awk -v nucleus=${nucleus} '{ if($5==nucleus) print}' > data/train_tmp.txt   
+  cat ${rna}/train_features_${struct}_${r_cut}.txt | awk -v nucleus=${nucleus} '{ if($5==nucleus) print}' > data/train_tmp.txt   
   ./scripts/train.R --verbose --ga_populations=68 --ga_cycles=1000 --output_parameters=data/ga_model_${nucleus}.txt data/train_tmp.txt
 fi

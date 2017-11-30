86ef8c50e4513def960c14ba9853c1a2b293991a
blairwhittington <blairjw@umich.edu>
2017-11-30 10:31:33 -0500

    Generalized training scripts

---
+++ LarmordD_New/1NC0/job_temp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -A frank
+#SBATCH --job-name=1NC0
+#SBATCH --time=3:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+
+
+module load rosetta
+export ROSETTA=/export/apps/rosetta/src/rosetta_src_2016.32.58837_bundle
+source ~/.bashrc
+bash /export/nVerde/users/blairjw/frank_lab/larmord_2.0/LarmordD_New/minimize_for_predictor.bash 1NC0
